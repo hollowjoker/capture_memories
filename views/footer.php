338ae1f757780a23772c10883a38bce94e83944e
Sam Esquejo <sam.esquejo@august99.com>
2019-06-16 12:57:02 +0800

    :sparkles: admin setup

--- views/footer.php
+++ views/footer.php
@@ -3,5 +3,7 @@
 ?>
 
 		
+		<script src="<?= URL ?>public/js/jquery.min.js"></script>
+		<script src="<?= URL ?>public/js/bootstrap.min.js"></script>
 	</body>
 </html>
--- views/header.php
+++ views/header.php
@@ -8,9 +8,30 @@
 		<title>Capture the Memories</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="<?= URL ?>public/css/bootstrap.css">
-		<script src="<?= URL ?>public/js/jquery.min.js"></script>
-		<script src="<?= URL ?>public/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="<?= URL ?>public/css/bootstrap.min.css">
 	</head>
 	<body>
-	sample
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand" href="#">CM</a>
+
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+				<form class="form-inline my-2 my-lg-0  mr-auto">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+				<ul class="navbar-nav mt-2 mt-lg-0">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
