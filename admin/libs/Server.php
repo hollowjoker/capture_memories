f9024737041a6d3cbe70685d2deb85a5e3528451
Sam Esquejo <sam.esquejo@august99.com>
2019-08-10 15:20:02 +0800

    :ambulance: add connection on server.php

--- admin/libs/Server.php
+++ admin/libs/Server.php
@@ -7,3 +7,15 @@
 	
 	ini_set('memory_limit', '-1');
 	ini_set('max_execution_time', 300);
+
+	// macos connection
+	define('DATABASE_HOST','localhost');
+	define('DATABASE_USER','root');
+	define('DATABASE_PASS','root');
+	define('DATABASE_NAME','capture_memories');
+
+	// windows connection
+	// define('DATABASE_HOST','localhost');
+	// define('DATABASE_USER','root');
+	// define('DATABASE_PASS','');
+	// define('DATABASE_NAME','capture_memories');
