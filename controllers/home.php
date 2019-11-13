4a97d7dec1086e64b4eab14fef53b42f361f7aab
Carla Remo <mjcarlaremo@gmail.com>
2019-11-14 00:39:15 +0800

    landing page for reset and verify password

--- controllers/home.php
+++ controllers/home.php
@@ -11,23 +11,25 @@ class Home extends Controller
 	
 	public function index()
 	{
-		$this->view->render('views/home/verify.php', true);
-		exit;
-		$this->view->render('views/home/forget.php', true);
-		$this->view->render('views/home/mail.php', true);
 		$this->view->tours = $this->model->getTourData('domestic');
 		$this->view->international = $this->model->getTourData('international');
 		$this->view->render('views/home/index.php');
 	}
 	
 	public function mail() {
-		// $this->view->render('views/home/mail.php');
+		$this->view->render('views/home/mail.php', true);
 	}
 	public function forget() {
-		// $this->view->render('views/home/mail.php');
+		$this->view->render('views/home/forget.php', true);
 	}
 	public function verify() {
-		// $this->view->render('views/home/mail.php');
+		$this->view->render('views/home/verify.php', true);
+	}
+	public function reset_password() {
+		$this->view->render('views/home/reset_password.php');
+	}
+	public function verify_password() {
+		$this->view->render('views/home/verify_password.php');
 	}
 
 }
---
+++ views/home/reset_password.php
@@ -0,0 +1,20 @@
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3" style="justify-content: center;">
+                <div style="margin: 50px 0;">
+                    <h1>Forgot Your Password?</h1>
+                    <p> Enter your email address and we will send you a link to reset it.</p>
+                </div>
+                <form>
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" class="form-control" placeholder="Enter email">
+                </div>
+                <button type="submit" class="btn btn-primary">Reset Password</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
